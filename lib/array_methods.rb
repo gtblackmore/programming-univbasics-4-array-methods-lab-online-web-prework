cb8137390e858198c41dbcfe330864e2a5a4dd88
George Blackmore <gtblackmore@gmail.com>
2019-12-08 16:54:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def using_include(array, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element1 = "wow"
+  element2 = "Tardis"
+  array.include?("wow")
+  array.include?("Tardis")
 end
 
 def using_sort(array)
